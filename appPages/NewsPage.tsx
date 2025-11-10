f7f249870af7da31ef66faf0288f852ff0b189dc
Norman Cade <norman_cade32@yahoo.com>
2025-11-10 04:55:06 -0500

    Fix track function causing page loading error

--- appPages/NewsPage.tsx
+++ appPages/NewsPage.tsx
@@ -1,9 +1,7 @@
-import { track } from "@/lib/analytics";
 import NewsContent from "@/sections/news/NewsContent";
 import React from "react";
 
 const NewsPage = () => {
-  track("opened_news_page");
   return (
     <div className="page">
       <NewsContent />
