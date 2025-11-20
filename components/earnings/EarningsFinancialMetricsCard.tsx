8357d98414f264b7455c54d208ba02cc562b355d
Norman Cade <norman_cade32@yahoo.com>
2025-11-20 04:35:30 -0500

    fixed numbers appearing as NAN

--- components/earnings/EarningsFinancialMetricsCard.tsx
+++ components/earnings/EarningsFinancialMetricsCard.tsx
@@ -8,12 +8,17 @@ interface FinancialMetricsCardProps {
 
 const FinancialMetricsCard = ({ kpi }: FinancialMetricsCardProps) => {
   const { name, value, unit, yoyDelta, qoqDelta } = kpi;
+  // Remove $ and parenthesis from value if present
+  const cleanValue = value.replace(/[\$,()]/g, "");
+
   return (
     <div className="bg-(--background) rounded-lg p-2 flex flex-col gap-1 justify-between">
       <h3 className="font-bold text-xs md:text-base">{name}</h3>
       <p className="text-(--secondary-text-color) text-xs md:text-base">
-        {unit?.includes("USD") ? formatToUSD(parseInt(value)) : formatNumber(parseInt(value))}{" "}
-        {unit}
+        {unit?.includes("USD")
+          ? formatToUSD(parseInt(cleanValue))
+          : formatNumber(parseInt(cleanValue))}
+        {unit === "percent" ? "%" : ` ${unit}`}
       </p>
       <div className="flex gap-2">
         {yoyDelta && (
